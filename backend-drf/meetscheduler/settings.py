1cbc34fa4d5fa6c8bae036453ae6ab0fba3857e8
Sushank-Code <sushanklamsal7979@gmail.com>
2026-04-08 20:23:09 +0545

    Social Aith -Google

--- backend-drf/meetscheduler/settings.py
+++ backend-drf/meetscheduler/settings.py
@@ -172,7 +172,7 @@ DJOSER = {
     'TOKEN_MODEL' : None,  
 
     'SOCIAL_AUTH_TOKEN_STRATEGY': 'djoser.social.token.jwt.TokenStrategy',        
-    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ["http://127.0.0.1:8000/api/v1/auth/o/google-oauth2/","http://localhost:3000"],
+    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ["http://127.0.0.1:8000","http://localhost:3000"],
 
     'SERIALIZERS' :{
         'user': 'accounts.serializers.UserSerializer',
